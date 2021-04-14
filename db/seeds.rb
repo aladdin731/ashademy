adc5a35b715723c598b8dd1fec98cd3f198816b8
aladdin731 <aladdin731@hotmail.com>
2021-04-14 09:42:15 -0700

    change seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -198,21 +198,21 @@ Tag.destroy_all
     Request.destroy_all
     Request.create!(
       course_id: (Course.all)[4].id,
-      mentee_id: u6.id,
+      mentee_id: u1.id,
       start_time: Date.parse("#{last_year.to_s}-01-01"),
       end_time: Date.parse("#{last_year.to_s}-01-02"),
       status: 'APPROVED'
     )
     Request.create!(
       course_id: (Course.all)[0].id,
-      mentee_id: u6.id,
+      mentee_id: u4.id,
       start_time: Date.parse("#{last_year.to_s}-01-01"),
       end_time: Date.parse("#{last_year.to_s}-01-02"),
       status: 'PENDING'
     )
     Request.create!(
       course_id: (Course.all)[1].id,
-      mentee_id: u6.id,
+      mentee_id: u4.id,
       start_time: Date.parse("#{last_year.to_s}-01-01"),
       end_time: Date.parse("#{last_year.to_s}-01-02"),
       status: 'PENDING'
